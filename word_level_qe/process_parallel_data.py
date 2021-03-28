50b1c425f6144f2ac7425793b32cc921690abb4e
Chunting Zhou <chuntinz@fb.com>
2021-03-28 13:39:14 -0700

    fix

--- word_level_qe/process_parallel_data.py
+++ word_level_qe/process_parallel_data.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import sentencepiece as spm
 import sys
-
+import numpy as np
 
 root = "/home/chuntinz/tir5/data/qe_wmt18_ende"
 opt_dir = os.path.join(root, "data2")
@@ -25,8 +25,20 @@ def spm(fin_path, fout_path):
 for split in ['corpus']:
     opt_split = 'para'
 
-    shutil.copy(os.path.join(root, "others", "{}.en".format(split)), os.path.join(opt_dir, "{}.en".format(opt_split)))
-    shutil.copy(os.path.join(root, "others", "{}.de".format(split)), os.path.join(opt_dir, "{}.ref".format(opt_split)))
+    en_fin = os.path.join(root, "others", "{}.en".format(split))
+    de_fin = os.path.join(root, "others", "{}.de".format(split))
+
+    en_sents = open(en_fin, "r", encoding="utf=8").readlines()
+    de_sents = open(de_fin, "r", encoding="utf=8").readlines()
+
+    K = 50000
+    rand_array = np.random.permutation(list(range(len(en_sents))))
+
+    with open(os.path.join(opt_dir, "{}.en".format(opt_split)), "w", encoding="utf=8") as fen, \
+            open(os.path.join(opt_dir, "{}.ref".format(opt_split)), "w", encoding="utf=8") as fde:
+        for ii in rand_array[:K]:
+            fen.write(en_sents[ii])
+            fde.write(de_sents[ii])
 
     spm(os.path.join(opt_dir, "{}.en".format(opt_split)), os.path.join(opt_dir, "{}.en.bpe".format(opt_split)))
     spm(os.path.join(opt_dir, "{}.ref".format(opt_split)), os.path.join(opt_dir, "{}.ref.bpe".format(opt_split)))
