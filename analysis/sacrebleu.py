c70144a0116413fc268fbfb420d0c791c5a79d5c
Chunting Zhou <chuntinz@fb.com>
2021-04-08 23:06:14 -0700

    fix

--- analysis/sacrebleu.py
+++ analysis/sacrebleu.py
@@ -1,5 +1,15 @@
 import sys
 import sacrebleu
 
-fref = sys.argv[1]
-f
+fsys = sys.argv[1]
+fref = sys.argv[2]
+
+refs = []
+sys = []
+
+with open(fref, "r", encoding="utf-8") as fin_ref, open(fsys, "r", encoding="utf-8") as fin_sys:
+    for lref, lsys in zip(fin_ref, fin_sys):
+        refs.append([lref.strip()])
+        sys.append(lsys.strip())
+
+print(sacrebleu.corpus_bleu(sys, refs).score)
