61f3a5fbc03e02b0b167d586f5e2097032bb5efd
Chunting Zhou <chuntinz@fb.com>
2021-04-09 00:21:26 -0700

    fix

--- analysis/sacre_bleu.py
+++ analysis/sacre_bleu.py
@@ -9,8 +9,8 @@ sys = []
 
 with open(fref, "r", encoding="utf-8") as fin_ref, open(fsys, "r", encoding="utf-8") as fin_sys:
     for lref, lsys in zip(fin_ref, fin_sys):
-        refs.append([lref.strip()])
+        refs.append(lref.strip())
         sys.append(lsys.strip())
 
-bleu = sacrebleu.corpus_bleu(sys, refs)
+bleu = sacrebleu.corpus_bleu(sys, [refs])
 print(bleu.score)
